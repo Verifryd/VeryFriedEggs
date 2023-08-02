d485f0f3747156f0a25c0ac995a7014fa86a83c8
Verifryd <verifryd.video@gmail.com>
2023-08-02 18:52:46 -0400

    scriptupd

--- script.js
+++ script.js
@@ -4,18 +4,12 @@ document.getElementById('productImage').addEventListener('click', function() {
     var video = document.getElementById('productVideo');
     video.style.display = 'block';
     video.play();
-});
-
 document.getElementById('fabergeImage').addEventListener('click', function() {
     // Change the Fabergé Egg image to the EarlyEggcessA GIF
     document.getElementById('fabergeImage').src = 'assets/EarlyEggcessA.gif';
-});
-
 document.getElementById('kringleImage').addEventListener('click', function() {
     // Change the Kringle Kronic image to the EarlyEggcessB GIF
     document.getElementById('kringleImage').src = 'assets/EarlyEggcessB.gif';
-});
-
 document.getElementById('dpteImage').addEventListener('click', function() {
     // Change the DPtE image to the EarlyEggcessC GIF
     document.getElementById('dpteImage').src = 'assets/EarlyEggcessC.gif';
