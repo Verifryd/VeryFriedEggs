8f99bff15f7db7b8b0de3dae0993adff8294695b
Verifryd <verifryd.video@gmail.com>
2023-08-02 22:26:30 -0400

    okaaaay

--- script.js
+++ script.js
@@ -15,6 +15,40 @@ if (purchaseButton) {
     });
 }
 
+var purchaseButton = document.getElementById('purchaseButton');
+if (purchaseButton) {
+    purchaseButton.addEventListener('click', function() {
+        // Hide the product image and show the video player
+        document.getElementById('kringleImage').style.display = 'none';
+        var video = document.getElementById('productVideo');
+        video.style.display = 'block';
+        video.play();
+
+        // When the video ends, change the Fabergé Egg image to the EarlyEggcessA GIF
+        video.addEventListener('ended', function() {
+            document.getElementById('fabergeImage').src = 'assets/EarlyEggcessB.gif';
+            document.getElementById('fabergeImage').style.display = 'block';
+        });
+    });
+}
+
+var purchaseButton = document.getElementById('purchaseButton');
+if (purchaseButton) {
+    purchaseButton.addEventListener('click', function() {
+        // Hide the product image and show the video player
+        document.getElementById('DPtEImage').style.display = 'none';
+        var video = document.getElementById('productVideo');
+        video.style.display = 'block';
+        video.play();
+
+        // When the video ends, change the Fabergé Egg image to the EarlyEggcessA GIF
+        video.addEventListener('ended', function() {
+            document.getElementById('fabergeImage').src = 'assets/EarlyEggcessC.gif';
+            document.getElementById('fabergeImage').style.display = 'block';
+        });
+    });
+}
+
 document.getElementById('productVideo').addEventListener('ended', function() {
     // Enable the "Next Egg" button when the video has ended
     document.getElementById('nextButton').disabled = false;
