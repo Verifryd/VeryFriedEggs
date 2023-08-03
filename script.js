10e9299bde174251274f2768e9d7f53010010343
Verifryd <verifryd.video@gmail.com>
2023-08-02 23:14:18 -0400

    hf

--- script.js
+++ script.js
@@ -64,5 +64,52 @@ window.onload = function() {
     // your code here
 };
 
+var fabergePurchase = document.getElementById('fabergePurchase');
+var kringlePurchase = document.getElementById('kringlePurchase');
+var dptePurchase = document.getElementById('dptePurchase');
+var productVideo = document.getElementById('productVideo');
+
+if (fabergePurchase && productVideo) {
+    fabergePurchase.addEventListener('click', function() {
+        document.getElementById('fabergeImage').style.display = 'none';
+        productVideo.style.display = 'block';
+        productVideo.play();
+
+        productVideo.addEventListener('ended', function() {
+            document.getElementById('fabergeImage').src = 'assets/EarlyEggcessA.gif';
+            document.getElementById('fabergeImage').style.display = 'block';
+            document.getElementById('nextButton').disabled = false;
+        });
+    });
+}
+
+if (kringlePurchase && productVideo) {
+    kringlePurchase.addEventListener('click', function() {
+        document.getElementById('kringleImage').style.display = 'none';
+        productVideo.style.display = 'block';
+        productVideo.play();
+
+        productVideo.addEventListener('ended', function() {
+            document.getElementById('kringleImage').src = 'assets/EarlyEggcessB.gif';
+            document.getElementById('kringleImage').style.display = 'block';
+            document.getElementById('nextButton').disabled = false;
+        });
+    });
+}
+
+if (dptePurchase && productVideo) {
+    dptePurchase.addEventListener('click', function() {
+        document.getElementById('DPtEImage').style.display = 'none';
+        productVideo.style.display = 'block';
+        productVideo.play();
+
+        productVideo.addEventListener('ended', function() {
+            document.getElementById('DPtEImage').src = 'assets/EarlyEggcessC.gif';
+            document.getElementById('DPtEImage').style.display = 'block';
+            document.getElementById('nextButton').disabled = false;
+        });
+    });
+}
+
 
 
