60c27ff4fdcbd82d7a5eaca32123c5e17d291a55
Verifryd <verifryd.video@gmail.com>
2023-08-02 22:33:56 -0400

    adds

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-var fabergeButton = document.getElementById('fabergeButton');
-if (fabergeButton) {
-    fabergeButton.addEventListener('click', function() {
-        document.getElementById('fabergeImage').style.display = 'none';
-        var video = document.getElementById('productVideo');
-        video.style.display = 'block';
-        video.play();
+document.getElementById('purchaseButton').addEventListener('click', function() {
+    // Hide the product image and show the video player
+    document.getElementById('fabergeImage').style.display = 'none';
+    var video = document.getElementById('productVideo');
+    video.style.display = 'block';
+    video.play();
 
-        video.addEventListener('ended', function() {
-            document.getElementById('fabergeImage').src = 'assets/EarlyEggcessA.gif';
-            document.getElementById('fabergeImage').style.display = 'block';
-            document.getElementById('nextButton').disabled = false;
-        });
+    // When the video ends, change the Fabergé Egg image to the EarlyEggcessA GIF
+    // and hide the video
+    video.addEventListener('ended', function() {
+        document.getElementById('fabergeImage').src = 'assets/EarlyEggcessA.gif';
+        document.getElementById('fabergeImage').style.display = 'block';
+        video.style.display = 'none';
     });
-}
+});
 
-var kringleButton = document.getElementById('kringleButton');
-if (kringleButton) {
-    kringleButton.addEventListener('click', function() {
-        document.getElementById('kringleImage').style.display = 'none';
-        var video = document.getElementById('productVideo');
-        video.style.display = 'block';
-        video.play();
+document.getElementById('purchaseButton').addEventListener('click', function() {
+    // Hide the product image and show the video player
+    document.getElementById('kringleImage').style.display = 'none';
+    var video = document.getElementById('productVideo');
+    video.style.display = 'block';
+    video.play();
 
-        video.addEventListener('ended', function() {
-            document.getElementById('kringleImage').src = 'assets/EarlyEggcessB.gif';
-            document.getElementById('kringleImage').style.display = 'block';
-            document.getElementById('nextButton').disabled = false;
-        });
+    // When the video ends, change the kringle Egg image to the EarlyEggcessA GIF
+    // and hide the video
+    video.addEventListener('ended', function() {
+        document.getElementById('kringleImage').src = 'assets/EarlyEggcessB.gif';
+        document.getElementById('kringleImage').style.display = 'block';
+        video.style.display = 'none';
     });
-}
+});
 
-var dpteButton = document.getElementById('dpteButton');
-if (dpteButton) {
-    dpteButton.addEventListener('click', function() {
-        document.getElementById('DPtEImage').style.display = 'none';
-        var video = document.getElementById('productVideo');
-        video.style.display = 'block';
-        video.play();
+document.getElementById('purchaseButton').addEventListener('click', function() {
+    // Hide the product image and show the video player
+    document.getElementById('DPtEImage').style.display = 'none';
+    var video = document.getElementById('productVideo');
+    video.style.display = 'block';
+    video.play();
 
-        video.addEventListener('ended', function() {
-            document.getElementById('DPtEImage').src = 'assets/EarlyEggcessC.gif';
-            document.getElementById('DPtEImage').style.display = 'block';
-            document.getElementById('nextButton').disabled = false;
-        });
+    // When the video ends, change the DPtE Egg image to the EarlyEggcessA GIF
+    // and hide the video
+    video.addEventListener('ended', function() {
+        document.getElementById('DPtEImage').src = 'assets/EarlyEggcessB.gif';
+        document.getElementById('DPtEImage').style.display = 'block';
+        video.style.display = 'none';
     });
-}
+});
 
 document.getElementById('productVideo').addEventListener('ended', function() {
     // Enable the "Next Egg" button when the video has ended
