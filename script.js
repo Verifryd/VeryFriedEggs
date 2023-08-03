6cfa2bf890a24162ba91eb50de9906eb994c2bfa
Verifryd <verifryd.video@gmail.com>
2023-08-02 23:47:30 -0400

    fkhn

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ document.getElementById('purchaseButton').addEventListener('click', function() {
     });
 });
 
-document.getElementById('purchaseButton').addEventListener('click', function() {
+document.getElementById('kringlepurchaseButton').addEventListener('click', function() {
     // Hide the product image and show the video player
     document.getElementById('kringleImage').style.display = 'none';
-    var video = document.getElementById('productVideo');
+    var video = document.getElementById('kringleproductVideo');
     video.style.display = 'block';
     video.play();
 
@@ -30,33 +30,26 @@ document.getElementById('purchaseButton').addEventListener('click', function() {
     });
 });
 
+document.getElementById('dptepurchaseButton').addEventListener('click', function() {
+    // Hide the product image and show the video player
+    document.getElementById('dpteImage').style.display = 'none';
+    var video = document.getElementById('dpteproductVideo');
+    video.style.display = 'block';
+    video.play();
+
+    // When the video ends, change the Fabergé Egg image to the EarlyEggcessA GIF
+    // and hide the video
+    video.addEventListener('ended', function() {
+        document.getElementById('dpteImage').src = 'assets/EarlyEggcessC.gif';
+        document.getElementById('dpteImage').style.display = 'block';
+        video.style.display = 'none';
+    });
+});
+
 document.getElementById('productVideo').addEventListener('ended', function() {
     // Enable the "Next Egg" button when the video has ended
     document.getElementById('nextButton').disabled = false;
 });
-
-var purchaseButton = document.getElementById('purchaseButton');
-var kringlePurchaseButton = document.getElementById('kringlePurchaseButton');
-var dptePurchaseButton = document.getElementById('dptePurchaseButton');
-
-if (purchaseButton) {
-    purchaseButton.addEventListener('click', function() {
-        // Fabergé Egg functionality
-    });
-}
-
-if (kringlePurchaseButton) {
-    kringlePurchaseButton.addEventListener('click', function() {
-        // Kringle Kronic functionality
-    });
-}
-
-if (dptePurchaseButton) {
-    dptePurchaseButton.addEventListener('click', function() {
-        // Don't Press the Egg functionality
-    });
-}
-
 
 window.onload = function() {
     // your code here
