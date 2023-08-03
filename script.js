609a3393d66b58c38554e05ce9ee10a80c7c31c9
Verifryd <verifryd.video@gmail.com>
2023-08-02 22:05:07 -0400

    oka

--- script.js
+++ script.js
@@ -44,3 +44,20 @@ document.getElementById('purchaseButton').addEventListener('click', function() {
         document.getElementById('fabergeImage').style.display = 'block';
     });
 });
+
+document.getElementById('purchaseButton').addEventListener('click', function() {
+    // Hide the product image and show the video player
+    document.getElementById('fabergeImage').style.display = 'none';
+    var video = document.getElementById('productVideo');
+    video.style.display = 'block';
+    video.play();
+
+    // When the video ends, change the Fabergé Egg image to the EarlyEggcessA GIF
+    // and hide the video
+    video.addEventListener('ended', function() {
+        document.getElementById('fabergeImage').src = 'assets/EarlyEggcessA.gif';
+        document.getElementById('fabergeImage').style.display = 'block';
+        video.style.display = 'none';
+    });
+});
+
